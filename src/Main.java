4fa5109c231853481adc0b874b19aee4d560296f
JuliaR <donidonic@gmail.com>
2022-11-30 20:13:01 +0300

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 521;
+        int mile = 20;
+        int bonus = price / mile;
+
+        System.out.println("Ваши бонусные мили: " + bonus);
+    }
+}
